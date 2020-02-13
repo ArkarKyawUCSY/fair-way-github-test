6fc80259253b371f5be32d07da15ca7b771a4af8
Ye Lin Htet <yelinhtet2050@gmail.com>
2020-02-13 14:56:49 +0900

    java file added

---
+++ Boxer.java
@@ -0,0 +1,6 @@
+class Boxer {
+	
+	public static void main(String args[]) {
+		System.out.print("HEllo Boxer!!!!!!!!!11");
+	}
+}
---
+++ KickBoxer.java
@@ -0,0 +1,6 @@
+class KickBoxer {
+	
+	public static void main(String args[]) {
+		System.out.print("HEllo KickBoxer!!!!!!!!!11");
+	}
+}
